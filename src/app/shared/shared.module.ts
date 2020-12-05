c895a568339bec5ead66d8c26240769f3a5fd53c
AnaMaghear <ana.maghear@yahoo.com>
2020-12-05 16:20:52 +0200

    added menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MenuComponent} from './components/menu/menu.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [MenuComponent]
 })
-export class SharedModule { }
+export class SharedModule {
+}
