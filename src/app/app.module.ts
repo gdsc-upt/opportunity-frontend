e47d4070eb1a0a47cf8dc7362b75faa021da8645
DanielaRed <rednic.dana20@gmail.com>
2020-12-05 16:00:53 +0200

    add banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { BannerComponent } from './pages/about-page/components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
