5d3781da30397f6b5713a00a2903941602244946
Andreea Miculescu <andreeamiculescu9@gmail.com>
2020-12-05 16:28:14 +0200

    added about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {SharedModule} from './shared/shared.module';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { BannerComponent } from './pages/about-page/components/banner/banner.component';
 import { TeamComponent } from './pages/about-page/components/team/team.component';
+import { AboutComponent } from './pages/about-page/components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutPageComponent,
+    TeamComponent,
     BannerComponent,
-    TeamComponent
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
