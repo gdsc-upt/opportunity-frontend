146bf6be814886eabbb565f2ec0517c479e3b924
Dan Percic <danpercic86@gmail.com>
2020-12-06 21:37:17 +0200

    migrate project to scss

--- src/app/pages/about-page/components/banner/banner.component.ts
+++ src/app/pages/about-page/components/banner/banner.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
 
