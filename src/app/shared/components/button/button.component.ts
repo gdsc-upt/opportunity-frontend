fa2cc39693e009bf8a85fd9106a87fe54d6e9f6b
Dan Percic <danpercic86@gmail.com>
2021-01-31 14:16:24 +0200

    add mat icon to button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,19 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'app-button',
     templateUrl: './button.component.html',
     styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
     @Input() public small = false;
     @Input() public disabled = false;
     @Input() public fullWidth = false;
     @Input() public loading = false;
     @Input() public label: string;
+    @Input() public matIcon: string;
     @Output() public action = new EventEmitter<any>();
-
-    public constructor() {}
-
-    public ngOnInit(): void {}
 }
